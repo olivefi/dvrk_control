a336b4d99f909aac387f10a839d7df405cc603a5
olivefi <olivefi@ethz.ch>
2024-09-09 14:11:15 +0200

    tracking

--- include/dvrk_control/DVRKControl.hpp
+++ include/dvrk_control/DVRKControl.hpp
@@ -9,6 +9,7 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/WrenchStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <sensor_msgs/JointState.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Bool.h>
@@ -28,8 +29,11 @@ public:
 protected:
   // Methods
   geometry_msgs::WrenchStamped wrenchToDVRKFrame(const geometry_msgs::WrenchStamped &wrench, const geometry_msgs::TransformStamped &dvrkTrans);
-  geometry_msgs::TransformStamped dvrkToNormal(const geometry_msgs::TransformStamped &dvrkTrans);
+  geometry_msgs::TransformStamped dvrkPoseToNormal(const geometry_msgs::TransformStamped &dvrkTrans);
+  geometry_msgs::TwistStamped dvrkTwistToNormal(const geometry_msgs::TwistStamped &twist);
 
+  geometry_msgs::WrenchStamped createImpdCmd(const geometry_msgs::TransformStamped &currPose, const geometry_msgs::TransformStamped &desPose,
+                                const geometry_msgs::TwistStamped &currTwist, const geometry_msgs::TwistStamped &desTwist);
   // Coord frame transforms
   Eigen::Matrix3d dvrkCoordToNormalCoord_ = Eigen::Matrix3d::Identity();
   Eigen::Matrix3d normalCoordToDvrkCoord_ = Eigen::Matrix3d::Identity();
@@ -42,6 +46,8 @@ protected:
   // Internal variables
   geometry_msgs::TransformStamped dvrkPoseLeft_;
   geometry_msgs::TransformStamped dvrkPoseRight_;
+  geometry_msgs::TwistStamped dvrkTwistLeft_;
+  geometry_msgs::TwistStamped dvrkTwistRight_;
 
   geometry_msgs::TransformStamped desPoseLeft_;
   geometry_msgs::TransformStamped desPoseRight_;
@@ -50,25 +56,32 @@ protected:
   void dvrkPoseLeftCallback(const geometry_msgs::TransformStamped::ConstPtr &msg);
   void dvrkPoseRightCallback(const geometry_msgs::TransformStamped::ConstPtr &msg);
 
+  void dvrkTwistLeftCallback(const geometry_msgs::TwistStamped::ConstPtr &msg);
+  void dvrkTwistRightCallback(const geometry_msgs::TwistStamped::ConstPtr &msg);
+
   void controlPoseLeftCallback(const geometry_msgs::TransformStamped::ConstPtr &msg);
   void controlPoseRightCallback(const geometry_msgs::TransformStamped::ConstPtr &msg);
 
 
-  // Publishers + Subscribers
+  // Publishers
   ros::Publisher leftGravcompPub_;
   ros::Publisher rightGravcompPub_;
-
-  ros::Subscriber dvrkPoseLeftSub_;
-  ros::Subscriber dvrkPoseRightSub_;
-
-  ros::Subscriber desPoseLeftSub_;
-  ros::Subscriber desPoseRightSub_;
 
   ros::Publisher currPoseLeftPub_;
   ros::Publisher currPoseRightPub_;
 
   ros::Publisher dvrkLeftWrenchPub_;
   ros::Publisher dvrkRightWrenchPub_;
+
+  // Subscribers
+  ros::Subscriber dvrkPoseLeftSub_;
+  ros::Subscriber dvrkPoseRightSub_;
+
+  ros::Subscriber dvrkTwistLeftSub_;
+  ros::Subscriber dvrkTwistRightSub_;
+
+  ros::Subscriber desPoseLeftSub_;
+  ros::Subscriber desPoseRightSub_;
 
   ros::Publisher leftErrorPub_;
   ros::Publisher rightErrorPub_;
--- src/DVRKControl.cpp
+++ src/DVRKControl.cpp
@@ -20,6 +20,10 @@ bool DVRKControl::init() {
       "/MTML/measured_cp", 1, &DVRKControl::dvrkPoseLeftCallback, this);
   dvrkPoseRightSub_ = nh_->subscribe(
       "/MTMR/measured_cp", 1, &DVRKControl::dvrkPoseRightCallback, this);
+  dvrkTwistLeftSub_ = nh_->subscribe(
+      "/MTML/measured_cv", 1, &DVRKControl::dvrkTwistLeftCallback, this);
+  dvrkTwistRightSub_ = nh_->subscribe(
+      "/MTMR/measured_cv", 1, &DVRKControl::dvrkTwistRightCallback, this);
   desPoseLeftSub_ = nh_->subscribe(
       "left/pose_des", 1, &DVRKControl::controlPoseLeftCallback, this);
   desPoseRightSub_ = nh_->subscribe(
@@ -52,6 +56,19 @@ bool DVRKControl::init() {
   zeroWrench.wrench.torque.z = 0;
   dvrkLeftWrenchPub_.publish(zeroWrench);
   dvrkRightWrenchPub_.publish(zeroWrench);
+
+  geometry_msgs::TransformStamped zeroPose;
+  zeroPose.header.stamp = ros::Time::now();
+  zeroPose.transform.translation.x = -0.35;
+  zeroPose.transform.translation.y = 0.0;
+  zeroPose.transform.translation.z = -0.14;
+  zeroPose.transform.rotation.x = 1.0;
+  zeroPose.transform.rotation.y = 0.0;
+  zeroPose.transform.rotation.z = 0.0;
+  zeroPose.transform.rotation.w = 0.0;
+  desPoseLeft_ = zeroPose;
+  desPoseRight_ = zeroPose;
+
   // Start updating loop
   any_worker::WorkerOptions workerOptions;
   workerOptions.name_ = ros::this_node::getName() + std::string{"_broadcast"};
@@ -71,9 +88,52 @@ void DVRKControl::cleanup() {}
 
 bool DVRKControl::update(const any_worker::WorkerEvent &event) {
   // Update curr
-  currPoseLeftPub_.publish(dvrkToNormal(dvrkPoseLeft_));
-  currPoseRightPub_.publish(dvrkToNormal(dvrkPoseRight_));
+  currPoseLeftPub_.publish(dvrkPoseLeft_);
+  currPoseRightPub_.publish(dvrkPoseRight_);
+
+  geometry_msgs::WrenchStamped leftCmd = createImpdCmd(dvrkPoseLeft_, desPoseLeft_, dvrkTwistLeft_, geometry_msgs::TwistStamped());
+  geometry_msgs::WrenchStamped rightCmd = createImpdCmd(dvrkPoseRight_, desPoseRight_, dvrkTwistRight_, geometry_msgs::TwistStamped());
+  dvrkLeftWrenchPub_.publish(wrenchToDVRKFrame(leftCmd, dvrkPoseLeft_));
+  dvrkRightWrenchPub_.publish(wrenchToDVRKFrame(rightCmd, dvrkPoseRight_));
+
   return true;
+}
+
+geometry_msgs::WrenchStamped DVRKControl::createImpdCmd(const geometry_msgs::TransformStamped &currPose, const geometry_msgs::TransformStamped &desPose,
+                                           const geometry_msgs::TwistStamped &currTwist, const geometry_msgs::TwistStamped &desTwist) {
+  geometry_msgs::WrenchStamped cmd;
+  Eigen::Vector3d posErr, velErr;
+  posErr << desPose.transform.translation.x - currPose.transform.translation.x,
+      desPose.transform.translation.y - currPose.transform.translation.y,
+      desPose.transform.translation.z - currPose.transform.translation.z;
+  velErr << desTwist.twist.linear.x - currTwist.twist.linear.x,
+      desTwist.twist.linear.y - currTwist.twist.linear.y,
+      desTwist.twist.linear.z - currTwist.twist.linear.z;
+  Eigen::Vector3d posCmd  = pGainPos_ * posErr + dGainPos_ * velErr;
+  cmd.header.stamp = ros::Time::now();
+  cmd.wrench.force.x = posCmd(0);
+  cmd.wrench.force.y = posCmd(1);
+  cmd.wrench.force.z = posCmd(2);
+  Eigen::Quaterniond oriCurr, oriDes;
+  oriCurr.x() = currPose.transform.rotation.x;
+  oriCurr.y() = currPose.transform.rotation.y;
+  oriCurr.z() = currPose.transform.rotation.z;
+  oriCurr.w() = currPose.transform.rotation.w;
+  oriDes.x() = desPose.transform.rotation.x;
+  oriDes.y() = desPose.transform.rotation.y;
+  oriDes.z() = desPose.transform.rotation.z;
+  oriDes.w() = desPose.transform.rotation.w;
+  Eigen::AngleAxisd oriErr(oriCurr.toRotationMatrix().transpose() * oriDes.toRotationMatrix());
+  Eigen::Vector3d angvelErr;
+  angvelErr << desTwist.twist.angular.x - currTwist.twist.angular.x,
+      desTwist.twist.angular.y - currTwist.twist.angular.y,
+      desTwist.twist.angular.z - currTwist.twist.angular.z;
+  Eigen::Vector3d angvelCmd;
+  angvelCmd = pGainOri_ * oriErr.angle() * oriCurr.toRotationMatrix() * oriErr.axis() + dGainOri_ * angvelErr;
+  cmd.wrench.torque.x = angvelCmd(0);
+  cmd.wrench.torque.y = angvelCmd(1);
+  cmd.wrench.torque.z = angvelCmd(2);
+  return cmd;
 }
 
 geometry_msgs::WrenchStamped DVRKControl::wrenchToDVRKFrame(
@@ -104,7 +164,7 @@ geometry_msgs::WrenchStamped DVRKControl::wrenchToDVRKFrame(
   return dvrkWrench;
 }
 
-geometry_msgs::TransformStamped DVRKControl::dvrkToNormal(
+geometry_msgs::TransformStamped DVRKControl::dvrkPoseToNormal(
     const geometry_msgs::TransformStamped &dvrkTrans) {
   geometry_msgs::TransformStamped normalTrans = dvrkTrans;
   normalTrans.header = dvrkTrans.header;
@@ -126,14 +186,46 @@ geometry_msgs::TransformStamped DVRKControl::dvrkToNormal(
   return normalTrans;
 }
 
+geometry_msgs::TwistStamped DVRKControl::dvrkTwistToNormal(const geometry_msgs::TwistStamped& dvrkTwist){
+  geometry_msgs::TwistStamped normalTwist = dvrkTwist;
+  normalTwist.header = dvrkTwist.header;
+  Eigen::Vector3d linVel;
+  linVel << dvrkTwist.twist.linear.x, dvrkTwist.twist.linear.y, dvrkTwist.twist.linear.z;
+  linVel = dvrkCoordToNormalCoord_ * linVel;
+  Eigen::Vector3d angVel;
+  angVel << dvrkTwist.twist.angular.x, dvrkTwist.twist.angular.y, dvrkTwist.twist.angular.z;
+  angVel = dvrkCoordToNormalCoord_ * angVel;
+  normalTwist.twist.linear.x = linVel(0);
+  normalTwist.twist.linear.y = linVel(1);
+  normalTwist.twist.linear.z = linVel(2);
+  normalTwist.twist.angular.x = angVel(0);
+  normalTwist.twist.angular.y = angVel(1);
+  normalTwist.twist.angular.z = angVel(2);
+  return normalTwist;
+}
+
 void DVRKControl::dvrkPoseLeftCallback(
     const geometry_msgs::TransformStamped::ConstPtr &msg) {
-  dvrkPoseLeft_ = *msg;
+  geometry_msgs::TransformStamped dvrkPoseLeft = *msg;
+  dvrkPoseLeft_ = dvrkPoseToNormal(dvrkPoseLeft);
 }
 
 void DVRKControl::dvrkPoseRightCallback(
     const geometry_msgs::TransformStamped::ConstPtr &msg) {
-  dvrkPoseRight_ = *msg;
+  geometry_msgs::TransformStamped dvrkPoseRight = *msg;
+  dvrkPoseRight_ = dvrkPoseToNormal(dvrkPoseRight);
+}
+
+void DVRKControl::dvrkTwistLeftCallback(
+    const geometry_msgs::TwistStamped::ConstPtr &msg) {
+  geometry_msgs::TwistStamped dvrkTwistLeft = *msg;
+  dvrkTwistLeft_ = dvrkTwistToNormal(dvrkTwistLeft);
+}
+
+void DVRKControl::dvrkTwistRightCallback(
+    const geometry_msgs::TwistStamped::ConstPtr &msg) {
+  geometry_msgs::TwistStamped dvrkTwistRight = *msg;
+  dvrkTwistRight_ = dvrkTwistToNormal(dvrkTwistRight);
 }
 
 void DVRKControl::controlPoseLeftCallback(
